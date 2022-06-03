711300f9bfe3a6e94127e8d939aab11a011e76a2
bseufw <ChandlerX101@gmail.com>
2022-06-03 10:08:16 -0400

    scroller

---
+++ test/Player.java
@@ -0,0 +1,43 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Player here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Player extends Actor
+{
+    
+    static int speed = 2; 
+    static int originalX, originalY; 
+    static int worldX, worldY; 
+    /**
+     * Act - do whatever the Player wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        move();
+    }
+    
+    public void move(){
+        if(Greenfoot.isKeyDown("left")){
+            setRotation(180);
+            move(speed); 
+        }
+        if(Greenfoot.isKeyDown("right")){
+            setRotation(0);
+            move(speed); 
+        }
+        if(Greenfoot.isKeyDown("up")){
+            setRotation(270);
+            move(speed);
+        }
+        if(Greenfoot.isKeyDown("down")){
+            setRotation(90);
+            move(speed);
+        }
+    }
+}
